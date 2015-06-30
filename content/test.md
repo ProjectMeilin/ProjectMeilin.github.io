069b375c76dec91449f36e58a9f89019cb7f1bf0
kilfu0701 <kilfu0701@gmail.com>
2015-06-30 16:40:33 +0800

    init files

---
+++ content/test.md
@@ -0,0 +1,8 @@
+Title: Test
+Date: 2015-06-30 16:00
+Category: Python
+Tags: python
+Author: kilfu0701
+Summary: Test
+
+Test it
---
+++ pelicanconf.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'kilfu0701'
+SITENAME = u'徵音梅林'
+
+# Test for local, leave it blank.
+SITEURL = ''#'https://projectmeilin.github.io'
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Taipei'
+
+DEFAULT_LANG = u'zh-TW'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('Pelican', 'http://getpelican.com/'),
+    #('Python.org', 'http://python.org/'),
+    #('Jinja2', 'http://jinja.pocoo.org/'),
+    #('You can modify those links in your config file', '#'),
+)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = '/Users/fu/pelican-themes/pelican-cait-custom'
+
+DISQUS_SITENAME = 'meilin'
+
