70f94f05eb370bb51f914daee44ed729dcdb391f
kilfu0701 <kilfu0701@gmail.com>
2015-07-06 23:16:38 +0800

    rebuild output file

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ for i in "${all_lang[@]}"
 do
     #sed -i "s/DEFAULT_LANG = u'[a-zA-Z]\{2\}'/DEFAULT_LANG = '$i'/g" pelicanconf.py
     echo "build ${i} ...."
-    cp -pr pelicanconf_${i}.py pelicanconf.py
-    pelican content/${i} -o output_${i} -s pelicanconf.py
+    #cp -pr pelicanconf_${i}.py pelicanconf.py
+    pelican content/${i} -o output_${i} -s pelicanconf_${i}.py
     mv output_${i} output/${i}
     #rm -rf /var/www/html/linne_pelican/${i}
     #cp -pr output/${i} /var/www/html/linne_pelican/${i}
@@ -18,3 +18,4 @@ done
 
 cp index.html output/index.html
 cp -pr output/ja/theme output/theme
+rm -rf *.pyc
