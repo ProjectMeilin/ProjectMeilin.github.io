aef59a64aa5e7bfb5cb7def72ee0d929e6c374ac
kilfu0701 <kilfu0701@gmail.com>
2016-10-22 12:20:58 +0900

    Let readme file in gh-pages & master branch.

--- build.sh
+++ build.sh
@@ -7,15 +7,12 @@ mkdir output
 all_lang=( "en" "ja" "zh" )
 for i in "${all_lang[@]}"
 do
-    #sed -i "s/DEFAULT_LANG = u'[a-zA-Z]\{2\}'/DEFAULT_LANG = '$i'/g" pelicanconf.py
     echo "build ${i} ...."
-    #cp -pr pelicanconf_${i}.py pelicanconf.py
     pelican content/${i} -o output_${i} -s pelicanconf_${i}.py
     mv output_${i} output/${i}
-    #rm -rf /var/www/html/linne_pelican/${i}
-    #cp -pr output/${i} /var/www/html/linne_pelican/${i}
 done
 
 cp index.html output/index.html
 cp -pr output/ja/theme output/theme
+cp README.md output/README.md
 rm -rf *.pyc
